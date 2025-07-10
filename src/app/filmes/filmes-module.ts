ed0004564316b0664696e7ef081c14638bf2c4ca
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-07-10 00:14:04 -0300

    adicionando roteamento e cores no crud

---
+++ src/app/filmes/filmes-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FilmesRoutingModule } from './filmes-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FilmesRoutingModule
+  ]
+})
+export class FilmesModule { }
